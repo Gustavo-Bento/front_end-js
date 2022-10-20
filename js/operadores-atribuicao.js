1e0577ae24d0dce23236f45f903260c36a6a9d8e
Gustavo Ferreira <gfmb94@gmail.com>
2022-10-20 18:02:37 -0300

    Criando Operadores de Atribuição em JavaScript

---
+++ js/operadores-atribuicao.js
@@ -0,0 +1,89 @@
+function atAd() {
+    var x = 2
+    var y = 4
+    let resultado = x += y
+
+    document.getElementById("res-atAd").innerHTML = resultado
+    document.getElementById("conf-atAd").innerHTML = "Resultado ✔"
+}
+
+function atSub() {
+    var x = 5
+    var y = 2
+    let resultado = x -= y
+
+
+    document.getElementById("res-atSub").innerHTML = resultado
+    document.getElementById("conf-atSub").innerHTML = "Resultado ✔"
+}
+
+function atMulti() {
+    var x = 2
+    var y = 2
+    let resultado = x *= y
+
+    document.getElementById("res-atMult").innerHTML = resultado
+    document.getElementById("conf-atMult").innerHTML = "Resultado ✔"
+}
+
+function atDiv() {
+    var x = 10
+    var y = 2
+    let resultado = x /= y
+
+    document.getElementById("res-atDiv").innerHTML = resultado
+    document.getElementById("conf-atDiv").innerHTML = "Resultado ✔"
+}
+
+function atPot() {
+    var x = 5
+    var y = 2
+    let resultado = x **= y
+
+    document.getElementById("res-atPot").innerHTML = resultado
+    document.getElementById("conf-atPot").innerHTML = "Resultado ✔"
+}
+
+function atRes() {
+    var x = 7
+    var y = 2
+    let resultado = x %= y
+
+    document.getElementById("res-atRes").innerHTML = resultado
+    document.getElementById("conf-atRes").innerHTML = "Resultado ✔"
+}
+// Operadores Aritméticos JavaScript
+// + 	Adição
+// - 	Subtração
+// * 	Multiplicação
+// / 	Divisão
+// ** 	Potenciação
+// % 	Resto
+// ++ 	Incrementação
+// -- 	Decrementação
+
+// Operadores de Atribuição JavaScript
+// = 	Atribuição 	Ex: x = 1 	-> x é 1
+// += 	Adição		Ex: x += 1 	-> x = x + 1
+// -= 	Subtração	Ex: x -= 1	-> x = x - 1
+// *= 	Multiplic.	Ex: x *= 1	-> x = x * 1
+// /= 	Divisão		Ex: x /= 1	-> x = x / 1
+// **=	Potenciação 	Ex: x **= 1	-> x = x ** 1
+// %= 	Resto		Ex: x %= 1	-> x = x % 1
+
+
+// Operadores Comparativos JavaScript
+// == 	Igual
+// != 	Diferente
+// > 	Maior
+// < 	Menor
+// >= 	Maior ou Igual
+// <= 	Menor ou Igual
+
+// Operadores Lógicos JavaScript
+// E And	Ex: 4>5 && 10>5	Ex: 4>5 e 10>5 		-> Resultado Falso
+// OU Or 	Ex: 4>5 || 10>5 Ex: 4>5 ou 10>5 	-> Resultado Verdadeiro
+// Não Not Ex: !(4>5)	Ex: Não (4>5)		-> Resultado Verdadeiro
+// XOR 	Ex: 4>5 ^ 10>5 	Ex: 4>5 xor 10>5 	-> Resultado Verdadeiro
+
+
