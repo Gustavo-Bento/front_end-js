eb746a6978198928e0996d423b847a24fff54f71
Gustavo Ferreira <gfmb94@gmail.com>
2022-11-07 17:19:05 -0300

    Adicionando _app.js ao projeto

---
+++ pages/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/botao.css'
+import '../styles/estilo.css'
+import '../styles/layout.css'
+import '../styles/lista.css'
+import '../styles/tabela-clara.css'
+import '../style/tabela.css'
+import '../styles/bootstrap/font-awesome.css'
+import '../styles/bootstrap/style.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
