46161de4b9e503c767906380e67a075ce3d1a9ce
Gustavo Ferreira <gfmb94@gmail.com>
2022-11-07 16:58:50 -0300

    Adicionando template das aulas

---
+++ pages/pages/aulas/aula-04.js
@@ -0,0 +1,88 @@
+export default function Aula04(){
+    return(
+        <div>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Prof Gustavo Ferreira" />
+        <meta name="descripion" content="Página de introdução ao Desenvolviemento web - Aula 06" />
+        <title>Aula 04 - Implementando BootStrap</title>
+        <link rel="stylesheet" href="/estilo.css" type="text/css" />
+        <link rel="stylesheet" href="/botao.css" type="text/css" />
+        <link rel="stylesheet" href="/lista.css" type="text/css" />
+        <link rel="stylesheet" href="/bootstrap/style.css" type="text/css" />
+        <link rel="stylesheet" href="/tabela-clara.css" type="text/css" />
+        <link rel="icon" href="/icon.png" type="image/x-icon" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <header>
+          <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div className="container-fluid">
+              <ul className="navbar-nav container">
+                <li className="nav-item">
+                  <a className="navbar-brand" href="http://microcamp.com">
+                    <img src="/logo.jpg" style={{width: '60px'}} alt="Logo Microcamp" id="arredondar" />
+                  </a>
+                </li>
+                <li className="nav-item text-white">
+                  <h4>Aula 04 - Implementando BootStrap</h4>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <section className="row">
+          <nav className="col-lg-3 bg-purple">
+            <ul className="navbar-nav m-3">
+              <li className="nav-item">
+                <a href="aula-01.html">
+                  <div className="botao-lista">
+                    Aula 01 - Git com HTML e CSS
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="aula-02.html">
+                  <div className="botao-lista">
+                    Aula 02 - Seletores CSS 3
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="aula-03.html">
+                  <div className="botao-lista">
+                    Aula 03 - Introdução ao JavaScript
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="aula-04.html">
+                  <div className="botao-lista">
+                    Aula 04 - Implementando BootStrap
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="aula-05.html">
+                  <div className="botao-lista">
+                    Aula 05 - Operadores em JavaScript
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="aula-06.html">
+                  <div className="botao-lista">
+                    Aula 06 - Portas Lógicas em JavaScript
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <article className="col-lg-9">
+
+          </article>
+        </section>
+    <footer className="text-center p-2 bg-dark text-white">
+  <p>Desenvolvido por Gustavo Ferreira</p>
+</footer>
+</div>
+);
+}
